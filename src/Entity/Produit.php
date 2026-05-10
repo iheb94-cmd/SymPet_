f6e0642ea996b19176bc840ef7cc813272be3367
iheb94-cmd <iheb.amara@rades.r-iset.tn>
2026-05-10 13:55:43 +0100

    Ajout Google Auth (#2)
    Co-authored-by: IhebAmara <ihebamara94@gmail.com>

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\ProduitRepository;
-use BcMath\Number;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
